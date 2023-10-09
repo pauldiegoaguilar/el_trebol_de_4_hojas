b17421eb52215d6f64609cf77b320e6a47b9608c
agusetn26 <augusto.etn26@gmail.com>
2023-10-09 14:07:32 -0300

    termino login y sign-up

---
+++ views/sing-up.php
@@ -0,0 +1,30 @@
+<div class="container h-100 text-center form-group ">
+    <div class="row align-items-center h-100">
+        <div class="col-md-6 mx-auto">
+            <form class="p-4 rounded mb-4">
+                <div class="form-group my-4 text-start">
+                    <label for="email">Nombre</label>
+                    <input type="text" class="form-control" id="Nombre" placeholder="Ej: elmasprokespro" required>
+                </div>
+                <div class="form-group my-4 text-start">
+                    <label for="email">Correo Electrónico</label>
+                    <input type="email" class="form-control" id="email" placeholder="Correo Electrónico" required>
+                </div>
+                <div class="form-group my-3 text-start">
+                    <label for="phoneNumber">Telefono (Opcional)</label>
+                    <input type="tel" class="form-control rounded-0" id="phoneNumber" placeholder="Ejemplo: XX XXXX-XXXX" pattern="[0-9]{2} [0-9]{4}-[0-9]{4}">
+                </div>
+                <div class="form-group my-4 text-start">
+                    <label for="password">Contraseña</label>
+                    <input type="password" class="form-control" id="password" required>
+                </div>
+                <div class="form-group my-4 text-start">
+                    <label for="password">Contraseña</label>
+                    <input type="password" class="form-control" id="password" required>
+                </div>
+                <button class="btn btn-dark " type="button" aria-expanded="false">Crear Cuenta</button>
+                <p class="mt-3">¿Ya tienes una Cuenta? <a href="#" class="delate-a">Inicia Sesion</a></p>
+            </form>
+        </div>
+    </div>
+</div>
