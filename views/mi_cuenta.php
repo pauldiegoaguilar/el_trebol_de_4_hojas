1db483f8e80c69265b62134d5340fa59b96ce17c
SPYROBO <alejandrolunaalvarez2007@gmail.com>
2023-10-12 12:49:03 -0300

    cam,bios

--- views/mi_cuenta.php
+++ views/mi_cuenta.php
@@ -3,11 +3,206 @@
     <h1>Mi Cuenta</h1>
 </div>
 
-<div class="container py-4">
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 ">
+            <div class="col">
+                <div class="mt-5">
+                    <div class="d-flex w-100 justify-content-between align-items-center">
+                        <h6 class="text-body-secondary m-0">MIS DATOS</h6>
+                        <p class="m-0">editar</p>
+                    </div>
+                    <hr>
+                    <div class="mt-4">
+                        <div class="d-flex gap-2">
+                            <span class="material-symbols-outlined">
+                                account_circle
+                            </span>
+                            <p class="text-body-secondary">Paul Diego Aguilar</p>
+                        </div>
+                        <div class="d-flex gap-2">
+                            <span class="material-symbols-outlined">
+                                mail
+                            </span>
+                            <p class="text-body-secondary">sample@gmail.com</p>
+                        </div>
+                        <div class="d-flex gap-2">
+                            <span class="material-symbols-outlined">
+                                badge
+                            </span>
+                            <p class="text-body-secondary">23761245</p>
+                        </div>
+                        <div class="d-flex gap-2">
+                            <span class="material-symbols-outlined">
+                                call
+                            </span>
+                            <p class="text-body-secondary">11 1234-5678</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <div class="d-flex w-100 justify-content-between align-items-center">
+                        <h6 class="text-body-secondary m-0">MIS DIRECCIONES</h6>
+                        <p class="m-0">editar</p>
+                    </div>
+                    <hr>
+                    <div class="mt-4">
+                        <div class="d-flex gap-2">
+                            <span class="material-symbols-outlined">
+                                location_on
+                            </span>
+                            <p class="fw-bolder">Principal</p>
+                        </div>
+                        <div class="d-flex gap-2">
+                            <span class="material-symbols-outlined opacity-0">
+                                check_box_outline_blank
+                            </span>
+                            <p class="text-body-secondary">Calle 9876</p>
+                        </div>
+                        <div class="d-flex gap-2">
+                            <span class="material-symbols-outlined opacity-0">
+                                check_box_outline_blank
+                            </span>
+                            <p class="text-body-secondary">CABA, 1234 (CP)</p>
+                        </div>
+                        <div class="d-flex gap-2">
+                            <span class="material-symbols-outlined opacity-0">
+                                check_box_outline_blank
+                            </span>
+                            <p class="text-body-secondary">Capital Federal</p>
+                        </div>
+                        <div class="d-flex gap-2">
+                            <span class="material-symbols-outlined opacity-0">
+                                check_box_outline_blank
+                            </span>
+                            <p class="text-body-secondary">Argentina</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 mt-5">
+            <div class="d-flex w-100 justify-content-between align-items-center">
+                <h6 class="text-body-secondary m-0">MIS COMPRAS</h6>
+            </div>
+            <hr>
+            <div class="row">
+                <!-- <div class="col-md-6">.col-md-8</div> -->
+                <div class="col-md-6">                
+                    <div class="dropdown w-100">
+                        <button type="button" class="w-100 btn btn-success dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                            ORDEN #20087
+                        </button>
+                        <div class="dropdown-menu p-0">
+                            <div class="card w-100">
+                                <img src="img/demon10.png" class="card-img-top" alt="img">
+                                <div class="card-body">
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">SEGUI TU ORDEN</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">                
+                    <div class="dropdown w-100">
+                        <button type="button" class="w-100 btn btn-success dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                            ORDEN #20087
+                        </button>
+                        <div class="dropdown-menu p-0">
+                            <div class="card w-100">
+                                <img src="img/demon10.png" class="card-img-top" alt="img">
+                                <div class="card-body">
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">SEGUI TU ORDEN</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">                
+                    <div class="dropdown w-100">
+                        <button type="button" class="w-100 btn btn-success dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                            ORDEN #20087
+                        </button>
+                        <div class="dropdown-menu p-0">
+                            <div class="card w-100">
+                                <img src="img/demon10.png" class="card-img-top" alt="img">
+                                <div class="card-body">
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">SEGUI TU ORDEN</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="container">
+    <div class="d-flex">
+        <div class="d-flex flex-column">
+            <div class="d-flex w-100 justify-content-between align-items-center">
+                <h6 class="text-body-secondary m-0">MIS DATOS</h6>
+                <p class="m-0">editar</p>
+            </div>
+            <div class="">
+                <div class="d-flex">
+                    <span class="material-symbols-outlined">
+                        account_circle
+                    </span>
+                    <p>Paul Diego Aguilar</p>
+                </div>
+                <div class="d-flex">
+                    <span class="material-symbols-outlined">
+                        mail
+                    </span>
+                    <p>diegopaul483@gmail.com</p>
+                </div>
+                <div class="d-flex">
+                    <span class="material-symbols-outlined">
+                        badge
+                    </span>
+                    <p>94854989</p>
+                </div>
+                <div class="d-flex">
+                    <span class="material-symbols-outlined">
+                        call
+                    </span>
+                    <p>11 5136-2748</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex flex-column">
+            <div class="d-flex w-100 justify-content-between align-items-center">
+                <h6 class="text-body-secondary m-0">MIS COMPRAS</h6>
+            </div>
+            <div class="">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container">
     <!-- <h2 class="pb-2 border-bottom">Features with title</h2> -->
 
     <div class="row row-cols-1 row-cols-md-2 align-items-md-center g-5">
-        <div class="col d-flex flex-column align-items-start">
+        <div class="d-flex flex-column align-items-start">
             <div class="d-flex w-100 justify-content-between">
                 <h6 class="text-body-secondary">MIS DATOS</h6>
                 <p>editar</p>
@@ -17,48 +212,27 @@
             <a href="#" class="btn btn-primary btn-lg">Primary button</a>
         </div>
 
-        <div class="col">
+        <div class="d-flex flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+                <h6 class="text-body-secondary">MIS COMPRAS</h6>
+            </div>
             <div class="row row-cols-1 row-cols-sm-2 g-4">
                 <div class="col d-flex flex-column gap-2">
-                    <div
-                        class="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
-                        <svg class="bi" width="1em" height="1em">
-                            <use xlink:href="#collection"></use>
-                        </svg>
-                    </div>
                     <h4 class="fw-semibold mb-0 text-body-emphasis">Featured title</h4>
                     <p class="text-body-secondary">Paragraph of text beneath the heading to explain the heading.</p>
                 </div>
 
                 <div class="col d-flex flex-column gap-2">
-                    <div
-                        class="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
-                        <svg class="bi" width="1em" height="1em">
-                            <use xlink:href="#gear-fill"></use>
-                        </svg>
-                    </div>
                     <h4 class="fw-semibold mb-0 text-body-emphasis">Featured title</h4>
                     <p class="text-body-secondary">Paragraph of text beneath the heading to explain the heading.</p>
                 </div>
 
                 <div class="col d-flex flex-column gap-2">
-                    <div
-                        class="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
-                        <svg class="bi" width="1em" height="1em">
-                            <use xlink:href="#speedometer"></use>
-                        </svg>
-                    </div>
                     <h4 class="fw-semibold mb-0 text-body-emphasis">Featured title</h4>
                     <p class="text-body-secondary">Paragraph of text beneath the heading to explain the heading.</p>
                 </div>
 
                 <div class="col d-flex flex-column gap-2">
-                    <div
-                        class="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
-                        <svg class="bi" width="1em" height="1em">
-                            <use xlink:href="#table"></use>
-                        </svg>
-                    </div>
                     <h4 class="fw-semibold mb-0 text-body-emphasis">Featured title</h4>
                     <p class="text-body-secondary">Paragraph of text beneath the heading to explain the heading.</p>
                 </div>
